15a6d56835a1b25d32ab6122c18cc536099393cb
Manaia <aviana@neolaw.net.br>
2018-04-04 12:38:33 -0300

    init project

---
+++ tests/main.spec.js
@@ -0,0 +1,36 @@
+/*
+  Desafio Fizzbuzz
+
+  Escreva uma lib que receba um número e:
+
+  Se o número for divisível por 3, no lugar do número escreva 'Fizz' - Done
+  Se o número for divisível por 5, no lugar do número escreva 'Buzz' - Done
+  Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz' - Done
+  Se não for múltiplo de nada, retorna o número
+
+*/
+
+import { expect } from 'chai';
+import FizzBuzz from '../src/main';
+
+describe('FizzBuzz', () => {
+  it('should return `Fizz` when multiple of 3', () => {
+    expect(FizzBuzz(3)).to.be.equal('Fizz');
+    expect(FizzBuzz(6)).to.be.equal('Fizz');
+  });
+  it('should return `Buzz` when multiple of 5', () => {
+    expect(FizzBuzz(5)).to.be.equal('Buzz');
+    expect(FizzBuzz(10)).to.be.equal('Buzz');
+  });
+  it('should return `FizzBuzz` when multiple of 3 and 5', () => {
+    expect(FizzBuzz(15)).to.be.equal('FizzBuzz');
+    expect(FizzBuzz(30)).to.be.equal('FizzBuzz');
+  });
+  it('should return the number when non-multiple', () => {
+    expect(FizzBuzz(7)).to.be.equal(7);
+  });
+
+  it('should return 0 when 0', () => {
+    expect(FizzBuzz(0)).to.be.equal(0);
+  });
+});
