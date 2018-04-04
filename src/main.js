148fdf66f72f4ebb6ad9ff2cf2c5bb8d131b01bb
Manaia <aviana@neolaw.net.br>
2018-04-04 13:49:11 -0300

    criando smoke e rotinas de testes

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-// export const search = () => fetch('https://spotify.com');
+export const search = () => fetch('https://spotify.com');
 export const searchArtists = () => {};
 export const searchAlbuns = () => {};
 export const searchTracks = () => {};
 export const searchPlaylists = () => {};
+
+// Adicionar o nyc no no json na propriedade prepush!! 
