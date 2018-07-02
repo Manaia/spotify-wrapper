21c8451efad82000e6b4a3ac28f089b06805b7fc
Arthur Manaia <arthurnovais7@gmail.com>
2018-07-01 21:24:44 -0300

    criando testes para a busca de albums

--- src/album.js
+++ src/album.js
@@ -1,4 +1,2 @@
-export const getAlbum = id => 
-  fetch(`https://api.spotify.com/v1/albums/${id}`)
-    .then(data => data.json());
+export const getAlbum = id => fetch(`https://api.spotify.com/v1/albums/${id}`).then(data => data.json());
 export const getAlbumTracks = () => {};
