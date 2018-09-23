88bcdda87ecd69f219618e5bd8f2aa75623aaf0a
Arthur Manaia <arthurnovais7@gmail.com>
2018-09-23 00:33:12 -0300

    add example using umd and add authorization flow

--- src/config.js
+++ src/config.js
@@ -1,1 +1,9 @@
+const TOKEN_API = "BQDgMsbvH2QHMMMfWtHLrQOVQcDsgYe7Nharz_tK6juMInNbY5FOG9rbD567uGcaMCizBKs3ZlYeQ7NrqkkMeD3S5t6_icaCp1teUtvKXcHOoN8GOMYeUCkEd23zwJPN5Md5GCGpTvSTj1LISo_wHw";
+
 export const API_URL = `https://api.spotify.com/v1`;
+
+export const HEADERS = {
+  headers: {
+    Authorization: `Bearer ${TOKEN_API}`,
+  },
+};
